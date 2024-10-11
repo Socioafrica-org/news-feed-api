4e3e7a1d02ea06df0e950ad03f44875eb4317ce1
onukwilip <onukwilip@gmail.com>
2024-10-11 21:29:48 +0100

    Modified the endpoints to retrieve posts and comments, made it optional to pass user token

--- src/routes/comment.route.ts
+++ src/routes/comment.route.ts
@@ -4,15 +4,17 @@ import {
   edit_comment,
   get_comment,
 } from "../controllers/comment.controller";
-import { validate_token } from "../middlewares/token.middleware";
+import { decode_token, validate_token } from "../middlewares/token.middleware";
 
 const comment_router = Router();
+
+// * The decode_token middleware below decrypts and retrieves the value of the user token
+comment_router.get("/:comment_id", decode_token as any, get_comment as any);
 
 // * Middleware to authenticate user token
 comment_router.use(validate_token as any);
 
 comment_router.post("/", create_comment as any);
 comment_router.put("/:comment_id", edit_comment as any);
-comment_router.get("/:comment_id", get_comment as any);
 
 export default comment_router;
--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -5,20 +5,27 @@ import {
   get_posts,
 } from "../controllers/post.controller";
 import multer from "multer";
-import { validate_token } from "../middlewares/token.middleware";
+import { decode_token, validate_token } from "../middlewares/token.middleware";
 import { get_user_topics } from "../middlewares/get-user-topics.middleware";
 
 // * Configures the multer library to store the uploaded file data (including the bytes) in the application memory
 const upload = multer({ storage: multer.memoryStorage() });
 const post_router = Router();
 
+// * The decode_token middleware below decrypts and retrieves the value of the user token
+post_router.get("/:post_id", decode_token as any, get_post as any);
+// * The get user topics middleware decodes the user acces token to get the username and retrieves topics relating to said user
+post_router.post(
+  "/all",
+  decode_token as any,
+  get_user_topics as any,
+  get_posts as any
+);
+
 // * The validate_token middleware below authenticates the user token
 post_router.use(validate_token as any);
 
 // * The multer middleware in the request below processes uploaded files and adds them to the Express Js req.files object
 post_router.post("/", upload.array("images"), create_post as any);
-post_router.get("/:post_id", get_post as any);
-// * The get user topics middleware decodes the user acces token to get the username and retrieves topics relating to said user
-post_router.post("/all", get_user_topics as any, get_posts as any);
 
 export default post_router;
